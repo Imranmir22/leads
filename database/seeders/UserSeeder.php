24046a949a13b7dda5f8b87d58960001e91f07dd
imranmir22 <imranmir87543@gmail.com>
2024-08-25 12:01:02 +0530

    delete files

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
 class UserSeeder extends Seeder
 {
     /**
@@ -12,24 +14,11 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        $this->createUsers(null, 3); // Start with 3 top-level users
-    }
-
-    private function createUsers($parentId, $depth)
-    {
-        if ($depth == 0) {
-            return;
-        }
-
-        // Create a random number of users for this level
-        $numUsers = rand(2, 5);
-        for ($i = 0; $i < $numUsers; $i++) {
-            $user = User::factory()->create(['parent_id' => $parentId]);
-
-            // Randomly decide if this user should have children
-            if (rand(0, 1) == 1) {
-                $this->createUsers($user->id, $depth - 1);
-            }
-        }
+        User::updateOrCreate([
+            'name' =>'Admin',
+            'email' => 'test@example.com',
+            'phone' => '9990009898',
+            'password' => Hash::make('password')
+        ]);
     }
 }
